049882f3fff7fc1d69de156c29fbbc6a2ae53610
panat54083 <panat54083@gmail.com>
2021-08-21 09:45:11 -0700

    Add 3.1 % 3.2

--- P3_1.rb
+++ P3_1.rb
@@ -1,1 +1,5 @@
-puts 'Hello'
+puts 5.class
+puts 5.class.superclass
+puts 5.class.superclass.superclass
+puts 5.class.superclass.superclass.superclass
+puts 5.class.superclass.superclass.superclass.superclass
