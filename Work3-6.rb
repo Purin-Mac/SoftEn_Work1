fd3785d09a99a9736f03a8f00204d2df976332b3
ThatphumCpre <69152288+ThatphumCpre@users.noreply.github.com>
2021-08-22 00:55:23 -0700

    Add initial with many attribute
    we can use syntax to given any attribute

--- Work3-6.rb
+++ Work3-6.rb
@@ -1,33 +1,34 @@
 
 class Class
-    def attr_accessor_with_history(attr_name)
-        self.class_eval("def #{attr_name};@#{attr_name};end") #if calling attribute ruturn value of current state
-        self.class_eval %Q{
-            def #{attr_name}=(value)   #setter attribute 
-		@attr_history = Hash.new if @attr_history.nil? #if attribute not created before create 		
-                if @attr_history["#{attr_name}"].nil?	 #if that attribute not in hash hash["attribute"] or nil
-			temp = Array.new() 	#create temp array
-			temp << value		#insert value to temp array
-			@attr_history["#{attr_name}"] = temp  #add temp array to hash history
+    def attr_accessor_with_history(*attr_names)
+	attr_names.each do |attr_name|
+		self.class_eval("def #{attr_name};@#{attr_name};end") #if calling attribute ruturn value of current state
+		self.class_eval %Q{
+		    def #{attr_name}=(value)   #setter attribute 
+			@attr_history = Hash.new if @attr_history.nil? #if attribute not created before create 		
+		        if @attr_history["#{attr_name}"].nil?	 #if that attribute not in hash hash["attribute"] or nil
+				temp = Array.new() 	#create temp array
+				temp << value		#insert value to temp array
+				@attr_history["#{attr_name}"] = temp  #add temp array to hash history
+				
+
+			else 	
+				@attr_history["#{attr_name}"] << value  #if array already created insert new values to hash history
+			end 
+			@#{attr_name} = value	 #set current values to attribute 
 			
+		    end
 
-		else 	
-			@attr_history["#{attr_name}"] << value  #if array already created insert new values to hash history
-		end 
-		@#{attr_name} = value	 #set current values to attribute 
-		
-            end
-
-            def history(attr)
-                @attr_history[attr.to_s]   #if call history return attribute wanted history -> h
-            end
-        }
+		    def history(attr)
+		        @attr_history[attr.to_s]   #if call history return attribute wanted history -> h
+		    end
+		}
+	end
     end
 end
 
 class Foo
-    attr_accessor_with_history :bar
-    attr_accessor_with_history :gay
+    attr_accessor_with_history :bar, :gay
 end
 
 
