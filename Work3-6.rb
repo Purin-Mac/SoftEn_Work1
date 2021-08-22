9dac654c577210b7206daf1cd6f9345650f34eec
ThatphumCpre <69152288+ThatphumCpre@users.noreply.github.com>
2021-08-22 00:41:19 -0700

    Add extend comment for Multi-attribute history
    add more comment to easily understand

--- Work3-6.rb
+++ Work3-6.rb
@@ -1,25 +1,25 @@
 
 class Class
     def attr_accessor_with_history(attr_name)
-        self.class_eval("def #{attr_name};@#{attr_name};end") #if calling 
+        self.class_eval("def #{attr_name};@#{attr_name};end") #if calling attribute ruturn value of current state
         self.class_eval %Q{
-            def #{attr_name}=(value)
-		@attr_history = Hash.new if @attr_history.nil?		
-                if @attr_history["#{attr_name}"].nil?
-			temp = Array.new() 
-			temp << value
-			@attr_history["#{attr_name}"] = temp
+            def #{attr_name}=(value)   #setter attribute 
+		@attr_history = Hash.new if @attr_history.nil? #if attribute not created before create 		
+                if @attr_history["#{attr_name}"].nil?	 #if that attribute not in hash hash["attribute"] or nil
+			temp = Array.new() 	#create temp array
+			temp << value		#insert value to temp array
+			@attr_history["#{attr_name}"] = temp  #add temp array to hash history
 			
 
 		else 	
-			@attr_history["#{attr_name}"] << value
+			@attr_history["#{attr_name}"] << value  #if array already created insert new values to hash history
 		end 
-		@#{attr_name} = value
+		@#{attr_name} = value	 #set current values to attribute 
 		
             end
 
             def history(attr)
-                @attr_history[attr.to_s]  
+                @attr_history[attr.to_s]   #if call history return attribute wanted history -> h
             end
         }
     end
@@ -34,7 +34,6 @@ end
 f = Foo.new     # => #<Foo:0x127e678>
 f.bar = 3       # => 3
 f.bar = :wowzo  # => :wowzo
-puts f.bar
 f.bar = 'boo!'  # => 'boo!'
 f.gay = 6 
 puts "first"
