d68d44cb62750c7a37cf1ebe0dfe4d8cf62315d3
ThatphumCpre <69152288+ThatphumCpre@users.noreply.github.com>
2021-08-22 00:39:26 -0700

    Multi-Attribute Added sperately
    history can use many Multi-Attribute to Added sperately

--- Work3-6.rb
+++ Work3-6.rb
@@ -1,17 +1,25 @@
+
 class Class
     def attr_accessor_with_history(attr_name)
-        attr_name = attr_name.to_s
-        attr_history = attr_name + '_history'
-        self.class_eval("def #{attr_name};@#{attr_name};end")
+        self.class_eval("def #{attr_name};@#{attr_name};end") #if calling 
         self.class_eval %Q{
             def #{attr_name}=(value)
-                @#{attr_history} = [] if @#{attr_history}.nil?
-                @#{attr_history} << value
-                @#{attr_name} = value
+		@attr_history = Hash.new if @attr_history.nil?		
+                if @attr_history["#{attr_name}"].nil?
+			temp = Array.new() 
+			temp << value
+			@attr_history["#{attr_name}"] = temp
+			
+
+		else 	
+			@attr_history["#{attr_name}"] << value
+		end 
+		@#{attr_name} = value
+		
             end
 
-            def history(#{attr_name})
-                @#{attr_history}
+            def history(attr)
+                @attr_history[attr.to_s]  
             end
         }
     end
@@ -19,14 +27,17 @@ end
 
 class Foo
     attr_accessor_with_history :bar
+    attr_accessor_with_history :gay
 end
+
 
 f = Foo.new     # => #<Foo:0x127e678>
 f.bar = 3       # => 3
-puts f.bar
 f.bar = :wowzo  # => :wowzo
 puts f.bar
 f.bar = 'boo!'  # => 'boo!'
-puts f.bar
-print f.history(:bar) # => [3, :wowzo, 'boo!']
-print "\n"
+f.gay = 6 
+puts "first"
+puts f.history(:bar).class # => [3, :wowzo, 'boo!']
+puts "second"
+puts f.history (:gay) 
