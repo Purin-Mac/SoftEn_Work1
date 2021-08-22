5e853b0f47da130b476e73a7b7e9206a9800f6b9
panat54083 <panat54083@gmail.com>
2021-08-22 10:55:17 -0700

    Add

---
+++ Work3_14.rb
@@ -0,0 +1,18 @@
+def my_swap(a,b)
+    b,a = a,b
+end
+  
+class Foo
+    attr_accessor :a, :b
+    def my_swap_2()
+        @b,@a = @a,@b
+    end
+end
+  
+def my_string_replace_1(s)
+    s.gsub( /Hi/, 'Hello')
+end
+  
+def my_string_replace_2(s)
+    s.gsub!( /Hi/, 'Hello')
+end
